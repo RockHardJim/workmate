d3e01e31938e053e0d54630a4ed4f4af36858480
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-09-11 23:23:32 +0200

    Added Laravel Sanctum for API authentication Added model relationship Added fillables to the location model Finished Api Login Function

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Users\Profile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+    /***
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request){
+        $v = Validator::make($request->all(),[
+           'username' => 'required|min:3|max:30',
+           'password' => 'required|min:5|max:15'
+        ]);
+
+        if($v->fails()) {
+            return response()->json([
+                'status' => false,
+                'error' => 'Hi, please ensure your form has been filled in correctly'
+            ], 404);
+        } else {
+            $user = User::where('username', $request->username)->first();
+            if(!$user){
+                return response()->json([
+                    'status' => false,
+                    'error' => 'Hi, we could not find the user account you are trying to access'
+                ], 404);
+            } else {
+                if(!Auth::attempt(['username' => $request->username, 'password' => $request->password])){
+                    return response()->json([
+                        'status' => false,
+                        'error' => 'Hi, ensure you have entered correct login credentials'
+                    ], 500);
+                } else {
+                    $token = $user->createToken('authToken')->plainTextToken;
+                    $profile = Profile::where('user', $request->username)->first();
+
+                    if(!$profile):
+                        $profile = false;
+                    endif;
+
+                    return response()->json([
+                        'token' => $token,
+                        'type' => 'Bearer',
+                        'profile' => $profile
+                        ]);
+                }
+            }
+        }
+    }
+
+    public function register(Request $request){
+
+    }
+
+    public function logout(Request $request){
+
+    }
+}
