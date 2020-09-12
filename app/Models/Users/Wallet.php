ea24aab05a1323c0c0b683d2e33787cfdabb135a
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-09-12 05:55:35 +0200

    Added Laravel Sanctum for API authentication Added model relationship Added fillables to the location model Finished Api Login Function Started Registration Function Auth Api done Added User Controller for location and profile

---
+++ app/Models/Users/Wallet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Users;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $table = 'wallets';
+    protected $fillable = ['user', 'bounty'];
+}
