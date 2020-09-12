1c79c8b8ee015d89406b465af63264f2eb80d687
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-09-13 01:27:43 +0200

    Added Laravel Sanctum for API authentication Added model relationship Added fillables to the location model Finished Api Login Function Started Registration Function Auth Api done Added User Controller for location and profile Added migration files for bounties

---
+++ database/migrations/2020_09_12_231330_bounty_profiles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BountyProfiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bounty_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('bounty');
+            $table->string('challenge')->unique();
+            $table->string('task')->comment('This is the name of the task e.g -> Help Kids Study Better');
+            $table->longText('description');
+            $table->enum('path', ['information technology', 'education', 'health-care', 'botany', 'environmental', 'sports', 'unspecified'])->default('unspecified');
+            $table->integer('bounty')->default(0);
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bounty_profiles');
+    }
+}
