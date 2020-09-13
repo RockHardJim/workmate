db9d26ba68418c359674a9707a8920b461379f9f
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-09-13 09:56:06 +0200

    Added Laravel Sanctum for API authentication Added model relationship Added fillables to the location model Finished Api Login Function Started Registration Function Auth Api done Added User Controller for location and profile Added migration files for bounties

--- app/Http/Controllers/Api/BountyController.php
+++ app/Http/Controllers/Api/BountyController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+Use App\Models\Platform\Bounties\Bounty;
+use App\Models\Platform\Bounties\BountyTask;
+use App\Models\Platform\Bounties\BountySubscription;
+use App\Models\Platform\Bounties\BountyChallenge;
 
 
 class BountyController extends Controller
@@ -17,4 +21,9 @@ class BountyController extends Controller
     public function challenges($challenge){
 
     }
+
+    public function tasks($task){
+
+    }
+
 }
